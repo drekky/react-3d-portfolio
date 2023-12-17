3a96b9047e3e5b1cf746332eba2c3e56140257d0
drekky <130186719+drekky@users.noreply.github.com>
2023-12-16 20:21:49 -0500

    Adding fully functional project to repository

---
+++ src/components/index.js
@@ -0,0 +1,8 @@
+import Alert from "./Alert";
+import CTA from "./CTA";
+import HomeInfo from "./HomeInfo";
+import Loader from "./Loader";
+import Navbar from "./Navbar";
+
+
+export { Alert, CTA, HomeInfo, Loader, Navbar }
